54543a49b5b03461b55ebd12a710be06bb458307
Stuart Walsh <stu@ipng.org.uk>
2020-01-14 23:35:29 +0000

    more register page localisation

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -9,8 +9,16 @@ import registerTh from './register/th.json';
 import registerZhhans from './register/zhhans.json';
 import registerZhhant from './register/zhhant.json';
 
-import commonEn from './common/en.json';
 import commonDe from './common/de.json';
+import commonEn from './common/en.json';
+import commonEs from './common/es.json';
+import commonFr from './common/fr.json';
+import commonIt from './common/it.json';
+import commonPl from './common/pl.json';
+import commonPt from './common/pt.json';
+import commonTh from './common/th.json';
+import commonZhans from './common/zhhans.json';
+import commonZhant from './common/zhhant.json';
 
 const translations = {
     en: {
@@ -22,28 +30,36 @@ const translations = {
         common: commonDe
     },
     es: {
-        register: registerEs
+        register: registerEs,
+        common: commonEs
     },
     fr: {
-        register: registerFr
+        register: registerFr,
+        common: commonFr
     },
     it: {
-        register: registerIt
+        register: registerIt,
+        common: commonIt
     },
     pl: {
-        register: registerPl
+        register: registerPl,
+        common: commonPl
     },
     pt: {
-        register: registerPt
+        register: registerPt,
+        common: commonPt
     },
     th: {
-        register: registerTh
+        register: registerTh,
+        common: commonTh
     },
     zhhans: {
-        register: registerZhhans
+        register: registerZhhans,
+        common: commonZhans
     },
     zhhant: {
-        register: registerZhhant
+        register: registerZhhant,
+        common: commonZhant
     }
 };
 
